ac5d55976ef34fbaf80426c13b53dc4c73b546cb
telegnom <max@telegnom.org>
2015-11-16 20:12:24 +0100

    persistent keyholders

--- christian.py
+++ christian.py
@@ -61,12 +61,15 @@ class ServiceFunctions():
 class KeyFunctions():
 
     def __init__(self):
-        self.keyholders = ["max", "gnom", "test"]
+        self.keyholders = []
+        #TODO verfiy that file exists
+        self.keyfile="./storage/keys.txt"
+        with open(self.keyfile, 'r') as keyfile:
+            for line in keyfile:
+                self.keyholders.append(" ".join(line.split()))
 
     def ListKeys(self,channel,cb):
         """List current holders of hq keys"""
-        #TODO make keyholders persistant
-
         print("ListKeys")
         keyMessage = "All the keys are belong to: "
         keyMessage += ", ".join(self.keyholders)
@@ -82,9 +85,17 @@ class KeyFunctions():
 
     def ChangeKeyholders(self,channel,cb,oldholder,newholder):
         """This changes the channel topic"""
+        print("change keys")
+        if newholder in self.keyholders:
+            cb.say(channel, "Noooooo! No more than one key for "+newholder+"!")
+            return(False)
         if oldholder in self.keyholders:
             self.keyholders[self.keyholders.index(oldholder)] = newholder
             self.ListKeys(channel,cb)
+            with open(self.keyfile, 'w') as keyfile:
+                for holder in self.keyholders:
+                    print>>keyfile,holder
+
             return(True)
         else:
             cb.say(channel, oldholder+ " has no key, better luck next time!")
