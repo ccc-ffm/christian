6aa2ab639d9aa87dbb1a87608a11de0deebc763e
Fabian Stahl <fabian.m.stahl@posteo.de>
2015-11-21 18:34:56 +0100

    Move filehandling to separate class

--- eggs.py
+++ eggs.py
@@ -1,61 +1,29 @@
 """Some eastereggs just for fun"""
-import random, os
+
+from files import Filehandler
 
 class EasterEggs(object):
     """Easter Egg functions"""
 
     def __init__(self):
-        pass
-
-    @classmethod
-    def getrandomline(cls, filename):
-        """Return a random line from a file"""
-        #Random Selection from Jonathan Kupferman:
-        #http://www.regexprn.com/2008/11/read-random-line-in-large-file-in.html
-        #Open the file:
-
-        my_file = open(filename, 'r')
-
-        #Get the total file size
-        file_size = os.stat(filename)[6]
-
-        #seek to a place in the file which is a random distance away
-        #Mod by file size so that it wraps around to the beginning
-        my_file.seek((my_file.tell()+random.randint(0, file_size-1))%file_size)
-
-        #dont use the first readline since it may fall in the middle of a line
-        my_file.readline()
-
-        #this will return the next (complete) line from the file
-        line = my_file.readline()
-        my_file.close()
-        return line
-
-    @classmethod
-    def geturl(cls, filename):
-        """Read url from filename"""
-        my_file = open(filename, 'r')
-        url = my_file.readline()
-        my_file.close()
-        return url
-
+        self.fhandler = Filehandler()
 
     def darkwing(self, channel, callback):
         """Post a random line"""
         filename = "./mylines/darkwing.txt"
-        myline = self.getrandomline(filename)
+        myline = self.fhandler.getrandomline(filename)
         callback.say(channel, myline)
 
     def balu(self, channel, callback):
         """Post a random line"""
         filename = "./mylines/balu.txt"
-        myline = self.getrandomline(filename)
+        myline = self.fhandler.getrandomline(filename)
         callback.say(channel, myline)
 
     def raspel(self, channel, callback):
         """Post url to raspel"""
         filename = "./myurls/raspel.url"
-        url = self.geturl(filename)
+        url = self.fhandler.getcontent(filename)
         callback.say(channel, url)
 
 
--- service.py
+++ service.py
@@ -1,32 +1,23 @@
 """Service function class"""
-import os
+
+from files import Filehandler
 
 class ServiceFunctions(object):
     """Some helper functions"""
 
     def __init__(self):
-        pass
-
-    @classmethod
-    def gettext(cls, filename):
-        """Return filecontent"""
-        filecont = ""
-        if not os.path.isfile(filename):
-            return "Something went terribly wrong, better luck next time!"
-        with open(filename, 'r') as infile:
-            for line in infile:
-                filecont += line.strip() + "\n"
-        return filecont
+        self.fhandler = Filehandler()
 
     def donnerstag(self, channel, callback):
         """Tell about public meeting"""
-        callback.say(channel, self.gettext("./mylines/donnerstag.txt"))
+        callback.say(channel, \
+                self.fhandler.getcontent("./mylines/donnerstag.txt"))
 
     def help(self, user, channel, callback):
         """Paste URLs to help"""
         if channel[1:] == callback.factory.getChannel():
-            helptext = self.gettext("./mylines/help.txt")
+            helptext = self.fhandler.getcontent("./mylines/help.txt")
         else:
-            helptext = self.gettext("./mylines/help_public.txt")
+            helptext = self.fhandler.getcontent("./mylines/help_public.txt")
         callback.msg(user, helptext, 120)
 
