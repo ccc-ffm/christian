9a6d7a8769007edcd961c923f38d09ea946c083a
Fabian Stahl <fabian.m.stahl@posteo.de>
2015-11-21 19:35:42 +0100

    Check if file exists in separate method

--- modules/files.py
+++ modules/files.py
@@ -9,10 +9,18 @@ class Filehandler(object):
         pass
 
     @classmethod
+    def exists(cls, filename):
+        """Check if file exists"""
+        if not os.path.isfile(filename):
+            return False
+        else:
+            return True
+
+    @classmethod
     def getcontent(cls, filename):
         """Return full content"""
         filecont = ""
-        if not os.path.isfile(filename):
+        if not cls.exists(filename):
             return "Something went terribly wrong, better luck next time!"
         with open(filename, 'r') as infile:
             for line in infile:
@@ -23,23 +31,27 @@ class Filehandler(object):
     @classmethod
     def getrandomline(cls, filename):
         """Return a random line from a file"""
-        #Random Selection from Jonathan Kupferman:
-        #http://www.regexprn.com/2008/11/read-random-line-in-large-file-in.html
-        #Open the file:
+        if not cls.exists(filename):
+            return "Something went terribly wrong, better luck next time!"
 
-        my_file = open(filename, 'r')
+        else:
+            #Jonathan Kupferman:
+            #http://www.regexprn.com/2008/11/read-random-line-in-large-file-in.html
+            #Open the file:
 
-        #Get the total file size
-        file_size = os.stat(filename)[6]
+            my_file = open(filename, 'r')
 
-        #seek to a place in the file which is a random distance away
-        #Mod by file size so that it wraps around to the beginning
-        my_file.seek((my_file.tell()+random.randint(0, file_size-1))%file_size)
+            #Get the total file size
+            file_size = os.stat(filename)[6]
 
-        #dont use the first readline since it may fall in the middle of a line
-        my_file.readline()
+            #seek to a place in the file which is a random distance away
+            #Mod by file size so that it wraps around to the beginning
+            my_file.seek((my_file.tell()+random.randint(0, file_size-1))%file_size)
 
-        #this will return the next (complete) line from the file
-        line = my_file.readline()
-        my_file.close()
-        return line
+            #dont use the first readline since it may fall in the middle of a line
+            my_file.readline()
+
+            #this will return the next (complete) line from the file
+            line = my_file.readline()
+            my_file.close()
+            return line
