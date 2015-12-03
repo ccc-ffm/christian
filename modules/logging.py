6c3456cb51c7237e8d6dac88b7f7b5bd709b85ad
Fabian Stahl <fabian.m.stahl@posteo.de>
2015-12-03 21:17:00 +0100

    Cleanup

--- modules/logging.py
+++ modules/logging.py
@@ -1,43 +1,49 @@
-from syslog import *
-import datetime, sys
+"""Provides logging for modules and Botcor"""
+import datetime, sys, syslog
 
-class BotLog():
-    facility = LOG_LOCAL0
+class BotLog(object):
+    """Logging class"""
+
+    facility = syslog.LOG_LOCAL0
     logid = "christian"
     def __init__(self):
         if "--debug" in sys.argv:
             self.debugmode = True
         else:
             self.debugmode = False
-        openlog(self.logid,0,LOG_LOCAL0)
+        syslog.openlog(self.logid, 0, syslog.LOG_LOCAL0)
 
     def log(self, prio, mesg):
+        """Write to log"""
         if self.debugmode == True:
             time = datetime.datetime.today().strftime('%Y-%m-%dT%H-%M-%S')
-            print(time + ": " + mesg)
-        if prio in ["emerg", "alert", "crit", "err", "warning", "notice", "info", "debug",]:
+            print time + ": " + mesg
+        if prio in ["emerg", "alert", "crit", "err", "warning", \
+                "notice", "info", "debug",]:
             if prio == "emerg":
-                prio = LOG_EMERG
+                prio = syslog.LOG_EMERG
             elif prio == "alert":
-                prio = LOG_ALERT
+                prio = syslog.LOG_ALERT
             elif prio == "crit":
-                prio = LOG_CRIT
+                prio = syslog.LOG_CRIT
             elif prio == "err":
-                prio = LOG_ERR
+                prio = syslog.LOG_ERR
             elif prio == "warning":
-                prio = LOG_WARNING
+                prio = syslog.LOG_WARNING
             elif prio == "notice":
-                prio = LOG_NOTICE
+                prio = syslog.LOG_NOTICE
             elif prio == "info":
-                prio = LOG_INFO
+                prio = syslog.LOG_INFO
             elif prio == "debug":
-                prio = LOG_DEBUG
-            syslog(prio, mesg)
+                prio = syslog.LOG_DEBUG
+            syslog.syslog(prio, mesg)
         else:
             #if no valid prio was specified, log error
-            syslog(LOG_ERR, "Unknown loglevel '" + prio + "' with message '" + mesg + "'")
+            syslog.syslog(syslog.LOG_ERR, "Unknown loglevel '" + prio \
+                    + "' with message '" + mesg + "'")
 
     def debug(self, mesg):
+        """Print to Stdout if debugging is enabled"""
         if self.debugmode == True:
             time = datetime.datetime.today().strftime('%Y-%m-%dT%H-%M-%S')
-            print(time + ": " + mesg)
+            print time + ": " + mesg
