450bd934d0ff9a50b0594dbd1586716b21ef26a7
Fabian Stahl <fabian.m.stahl@posteo.de>
2016-05-29 17:57:24 +0200

    issue #60:refactoring, add signal handler

--- christian.py
+++ christian.py
@@ -14,11 +14,12 @@ from ConfigParser import SafeConfigParser
 
 #Import the bots we want to create
 from bots import Bot
-from modules import BotLog
+from utils import BotLog, Signalhandler
 
 LOG = BotLog()
 
 LOG.log("notice", "Christian started")
+
 
 class BotFactory(protocol.ClientFactory):
     """A factory for Bots.
@@ -41,27 +42,30 @@ class BotFactory(protocol.ClientFactory):
 
 if __name__ == '__main__':
 
-
     #read Serversettings from config file
-    PARSER = SafeConfigParser()
+    parser = SafeConfigParser()
 
-    PARSER.read('./config/network.cfg')
-    HOST=PARSER.get('network', 'hostname')
-    PORT=PARSER.getint('network', 'port')
+    parser.read('./config/network.cfg')
+    host=parser.get('network', 'hostname')
+    port=parser.getint('network', 'port')
 
     #Read channels from config file
-    PARSER.read('./config/channels.cfg')
-    CHANNELS = PARSER.items( 'channels' )
+    parser.read('./config/channels.cfg')
+    channels = parser.items( 'channels' )
     chan_list=[]
-    for key, channel in CHANNELS:
+
+    for key, channel in channels:
             chan_list.append(channel)
 
     #Factory
-    FACTORY = BotFactory(chan_list)
+    factory = BotFactory(chan_list)
 
     #connect
-    LOG.log("info", "connecting to "+HOST+" on port "+str(PORT))
-    reactor.connectSSL(HOST, PORT, FACTORY, ssl.ClientContextFactory())
+    LOG.log("info", "connecting to "+host+" on port "+str(port))
+    reactor.connectSSL(host, port, factory, ssl.ClientContextFactory())
+
+    sig = Signalhandler(factory)
+    reactor.addSystemEventTrigger('before', 'shutdown', sig.savestates)
 
     #run
     LOG.log("info", "starting reactor")
