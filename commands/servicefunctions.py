d2cf1017aa81b44db836a76702f502e0cb92dcb3
Fabian Stahl <fabian.m.stahl@posteo.de>
2016-07-20 20:55:17 +0200

    Use kwargs, fix linebreak

--- commands/servicefunctions.py
+++ commands/servicefunctions.py
@@ -1,12 +1,11 @@
 from utils import Filehandler
 
 class ServiceFunctions(object):
-    """Some helper functions"""
 
     def __init__(self):
         self.fhandler = Filehandler()
 
-    def donnerstag(self, channel, callback, msg=None, nck=None, hq=None, keys=None, pb=None):
+    def donnerstag(self, channel, callback, **kwargs):
         """Tell about public meeting"""
-        callback.say(channel, \
-                self.fhandler.getcontent("./mylines/donnerstag.txt"))
+        callback.say(channel,
+                     self.fhandler.getcontent("./mylines/donnerstag.txt"))
