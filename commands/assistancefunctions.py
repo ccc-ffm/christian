865cfc491bd151d4e83bc38e9a7ed9720b9c4585
Fabian Stahl <fabian.m.stahl@posteo.de>
2017-06-25 16:49:33 +0200

    issue #88: Add help function

--- commands/assistancefunctions.py
+++ commands/assistancefunctions.py
@@ -2,10 +2,15 @@ class AssistanceFunctions(object):
 
     def pad(self, channel, callback, msg=None, nck=None, pad=None, **kwargs):
         if len(msg) < 1:
-            callback.say(channel, "Syntax: !pad <name> [private]")
+            callback.say(channel, 'Syntax: !pad <name> [private]')
         elif msg[2] == 'private':
             pad_url='{0}{1}'.format(pad.getPrivateUrl(),msg[1])
             callback.say(channel,pad_url)
         else:
             pad_url='{0}{1}'.format(pad.getPublicUrl(),msg[1])
             callback.say(channel,pad.url)
+
+    def help(self, channel, callback, msg=None, nck=None, pad=None, **kwargs):
+        helpmsg = "!pad <name> [private] - Create a pad named <name> at chaospad.de .\ņ"
+        helpmsg += "When using the argument private the link returned does not contain the pad credentials .\n"
+        callback.msg(nck, helpmsg)
