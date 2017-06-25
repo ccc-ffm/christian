84a1dea37dc56767671beae84e00ef6cb8797472
Chaosmaster <gh@chaosv.de>
2017-06-25 23:54:19 +0200

    remove debug output

--- modules/dudle.py
+++ modules/dudle.py
@@ -5,7 +5,4 @@ class Dudle(object):
         self.baseurl = 'https://dudle.inf.tu-dresden.de/?create_poll='
 
     def getDudle(self, name, type='time', url=''):
-        print name
-        print type
-        print url
         return(self.baseurl + name + '&poll_type=' + type + '&poll_url=' + url)
