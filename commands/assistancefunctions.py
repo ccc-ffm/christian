6723f04c1c8ca893ba0925f71ba1f45f8dea5677
Fabian Stahl <fabian.m.stahl@posteo.de>
2017-06-25 17:13:51 +0200

    issue #88: Rename argument to private, fix help

--- commands/assistancefunctions.py
+++ commands/assistancefunctions.py
@@ -2,15 +2,15 @@ class AssistanceFunctions(object):
 
     def pad(self, channel, callback, msg=None, nck=None, pad=None, **kwargs):
         if len(msg) < 1:
-            callback.say(channel, 'Syntax: !pad <name> [private]')
-        elif msg[2] == 'private':
-            pad_url='{0}{1}'.format(pad.getPrivateUrl(),msg[1])
+            callback.say(channel, 'Syntax: !pad <name> [public]')
+        elif msg[2] == 'public':
+            pad_url='{0}{1}'.format(pad.getPublicUrl(),msg[1])
             callback.say(channel,pad_url)
         else:
-            pad_url='{0}{1}'.format(pad.getPublicUrl(),msg[1])
+            pad_url='{0}{1}'.format(pad.getPrivateUrl(),msg[1])
             callback.say(channel,pad.url)
 
     def help(self, channel, callback, msg=None, nck=None, pad=None, **kwargs):
-        helpmsg = "!pad <name> [private] - Create a pad named <name> at chaospad.de .\ņ"
-        helpmsg += "When using the argument private the link returned does not contain the pad credentials .\n"
+        helpmsg = "!pad <name> [public] - Create a pad named <name> at chaospad.de "
+        helpmsg += "When using the argument public the link returned does not contain the pad credentials.\n"
         callback.msg(nck, helpmsg)
