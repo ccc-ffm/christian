46fbb1b1ede458182fa700ce01e01bee30135a80
Fabian Stahl <fabian.m.stahl@posteo.de>
2017-06-25 17:17:45 +0200

    FIX

--- commands/assistancefunctions.py
+++ commands/assistancefunctions.py
@@ -3,11 +3,11 @@ class AssistanceFunctions(object):
     def pad(self, channel, callback, msg=None, nck=None, pad=None, **kwargs):
         if len(msg) < 1:
             callback.say(channel, 'Syntax: !pad <name> [public]')
-        elif msg[2] == 'public':
-            pad_url='{0}{1}'.format(pad.getPublicUrl(),msg[1])
+        elif msg[1] == 'public':
+            pad_url='{0}{1}'.format(pad.getPublicUrl(),msg[0]
             callback.say(channel,pad_url)
         else:
-            pad_url='{0}{1}'.format(pad.getPrivateUrl(),msg[1])
+            pad_url='{0}{1}'.format(pad.getPrivateUrl(),msg[0]
             callback.say(channel,pad.url)
 
     def help(self, channel, callback, msg=None, nck=None, pad=None, **kwargs):
